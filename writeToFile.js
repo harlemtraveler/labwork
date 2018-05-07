409af3ef4fb7f49e21d889dfc219bae866ffde92
Timothy Keaveny <timothykeaveny@Timothys-MacBook-Pro.local>
2018-05-07 10:34:45 -0400

    Initial commit for writeToFile function

--- writeToFile.js
+++ writeToFile.js
@@ -3,5 +3,23 @@ This function should take a string and write a file called
 studentGroups.txt to the directory from which it's called. You will need to require the
 node fs object and use the fs.writeFile method. Google it.
 */
-
+const express require('express');
 const fs = require('fs');
+const url = request('url');
+const logger = require('morgan');
+const bodyParser = require('bodyParser');
+const msg = "";
+const studentGroupsFile = 'studentGroups.txt'
+
+const app = express();
+
+function writeFile() {
+  fs.writeFile(studentGroups.txt, msg, (err) => {
+    if(err) throw err;
+    console.log(`File saved to ${studentGroupsFile}`);
+  })
+}
+
+modlue.exports = {
+  writeFile
+}
